41f020345fa7ec36d2f448f7d487fdd40758bf71
valebuyondo <buyondovale@gmail.com>
2023-06-15 14:21:16 -0700

    3-name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+
 int main(void)
+	/*
+	 * main functio
+	 */
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
