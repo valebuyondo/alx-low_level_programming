6da5fa57952675bb42ad4c189ef54168546f4af2
valebuyondo <buyondovale@gmail.com>
2023-06-15 15:00:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
- int main(void)
-	/*
-	 * main function
-	 *
-	 * return: 0 on sucess
-	 */
- {
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
- }
+
+	return (0);
+}
