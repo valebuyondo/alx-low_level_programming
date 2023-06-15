2cdff395bda4f88eb53751be18ef2570a7033927
valebuyondo <buyondovale@gmail.com>
2023-06-15 15:12:00 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ *
+ * return: 0 on Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n ");
+
+	return (0);
+}
