65825a61916249052863b20fa239d61953a64d7e
valebuyondo <buyondovale@gmail.com>
2023-06-16 14:00:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
-	printf("Size of long int: 4 byte(s)\n");
-	printf("Size of long long int: 8 byte(s)\n");
-	printf("Size of float: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
 
 	return (0);
 }
