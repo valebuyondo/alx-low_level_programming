c344eb367646248f3083d5ddc88c19b5b8db9b45
valebuyondo <buyondovale@gmail.com>
2023-06-17 05:51:01 -0700

    100-intel

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
+ * main - Entry point
  *
- * Return: Always(Success)
+ * Return: Always 0 (Success)
  */
- int main(void)
- {
- 	return(0);
- }
+int main(void)
+{
+	return (0);
+}
