1a3eaeb360ed845b1d4332b6713fece192443120
valebuyondo <buyondovale@gmail.com>
2023-06-17 13:59:18 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+
+	return (0);
+}
