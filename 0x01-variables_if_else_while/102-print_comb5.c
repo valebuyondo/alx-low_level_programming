39ed5661460a9a2b14ae5be5c3bd5e442e52e9de
valebuyondo <buyondovale@gmail.com>
2023-06-18 13:55:59 -0700

    101

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1; num2 <= 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar('0' + num1 / 10);
 			putchar('0' + num1 % 10);
