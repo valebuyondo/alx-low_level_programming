da19c2ea0980b8fc3279d0c661a2e5f221f9074f
valebuyondo <buyondovale@gmail.com>
2023-06-18 13:45:21 -0700

    104

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1; num2 <= 99; num2++)
+		{
+			putchar('0' + num1 / 10);
+			putchar('0' + num1 % 10);
+			putchar(' ');
+			putchar('0' + num2 / 10);
+			putchar('0' + num2 % 10);
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
