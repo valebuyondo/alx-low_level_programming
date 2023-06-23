c1846a1b2a6d3ee4febbcb4012b00130160eb6ed
valebuyondo <buyondovale@gmail.com>
2023-06-23 02:08:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,19 +5,18 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-
-	_putchar('\n');
-}
 int main(void)
 {
-  
+  void print_alphabet(void)
+  {
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                _putchar(c);
+        }
+
+        _putchar('\n');
+   }
     return (0);
 }
