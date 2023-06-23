c0bb282df5be06242afe41ddd4b71c1795e876e8
valebuyondo <buyondovale@gmail.com>
2023-06-23 02:51:59 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int a=1;
+	while(a <= 10)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define main_h
 void print_alphabet(void);
 int _putchar(char c);
+void print_alphabet_x10(void);
 #endif
