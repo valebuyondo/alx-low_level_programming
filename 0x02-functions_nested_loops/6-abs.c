07e851bdf43981ce0fbab8dae0c90e1bc035966e
valebuyondo <buyondovale@gmail.com>
2023-06-25 03:40:22 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - computes the absolute value of an integer.
+ *@r: the integer to be computed
+ *Return: returns -r if r is greter thareturns r otherwise
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	return (r);
+}
