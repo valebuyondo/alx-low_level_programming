0bcd98a662da335a7136b600452a69b27568943e
valebuyondo <buyondovale@gmail.com>
2023-06-26 12:24:46 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <string.h>
+/**
+ *_strlen - the unction
+ *@str: string to be checked
+ *Return: return the length of the string
+ */
 int _strlen(char *str)
 {
 	int len;
+
 	len = strlen(str);
 	return (len);
 }
