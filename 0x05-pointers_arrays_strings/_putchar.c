8c23bfc0eda1fd3f6213e908f756d33ec01635c7
valebuyondo <buyondovale@gmail.com>
2023-06-26 14:20:21 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,5 +1,13 @@
 #include <unistd.h>
-
-int _putchar(char str) {
-    return write(STDOUT_FILENO, &str, 1);
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
