fd3f9b8d1ca977c0d002f139444aaaef63797688
valebuyondo <buyondovale@gmail.com>
2023-06-27 14:51:53 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - prints the first half
+ * @str: the string to be printed as half
+ */
+
+void puts_half(char *str)
+{
+	int length_of_the_string = strlen(str);
+	int n = (length_of_the_string - 1) / 2;
+
+	if (length_of_the_string % 2 == 0)
+		n = length_of_the_string / 2;
+	else
+		n = (length_of_the_string - 1) / 2;
+	while (str[n] != '\0')
+	{
+		putchar(str[n]);
+		n++;
+	}
+	printf("\n");
+
+}
