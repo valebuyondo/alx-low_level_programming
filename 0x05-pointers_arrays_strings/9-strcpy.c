c7186089c092547dc2eb670a19879bdfc6616d0e
valebuyondo <buyondovale@gmail.com>
2023-06-27 15:57:19 -0700

    9-p

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - copies string
+ * @dest: destination after copying
+ * @src: source of the copy
+ * Return: return dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+
+}
