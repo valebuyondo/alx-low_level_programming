4edf0db13727bb5c810a6a681d2888b6d10b7eca
valebuyondo <buyondovale@gmail.com>
2023-07-06 09:04:21 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion_helper - helper function in sqr root calculation
  * _sqrt_recursion - calculates the square root of a number
  * @n: the number for which the root is calculated
  * @low: lowest of n
@@ -8,7 +7,9 @@
  * Return: returns the qsare root of n
  */
 int _sqrt_recursion_helper(int n, int low, int high);
-
+/**
+ * _sqrt_recursion_helper - helper function in sqr root calculation
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
