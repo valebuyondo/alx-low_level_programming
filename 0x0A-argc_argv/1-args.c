067758171cb23650cb7663a2e81ead92588b061f
valebuyondo <buyondovale@gmail.com>
2023-07-09 07:19:01 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - prints the number of arguments passed
+ * @argc: arguments passed on afunction
+ * @argv: argumetn vector
+ * Return: returns number of arguments passed
+ */
 int main(int argc, char __attribute__((__unused__))*argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
