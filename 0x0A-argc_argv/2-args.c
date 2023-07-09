f51f202b41ffeb0518857a32203e30c192ded8f0
valebuyondo <buyondovale@gmail.com>
2023-07-09 07:44:05 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the urgements 
+ * @argc: arguments count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, int *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
