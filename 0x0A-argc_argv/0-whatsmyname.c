1382d63a8f251f6c37637ccc849c177d330ee197
valebuyondo <buyondovale@gmail.com>
2023-07-09 07:03:47 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a name of the function
+ * @argc: counts the number of parameters in a funtion
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
