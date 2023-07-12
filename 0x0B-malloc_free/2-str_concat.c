7dc2cacbda3c0a13c9f886faba31d5fa6435c956
valebuyondo <buyondovale@gmail.com>
2023-07-12 12:47:00 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int n
 	char *concat;
-	unsigned int len = n, index;
+	unsigned int len, index;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -36,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	for (index = 0; s1[index]; index++)
 		concat[len++] = s1[index];
 
-	for (index = 0; s2[index] && index < n; index++)
+	for (index = 0; s2[index] && index < len; index++)
 		concat[len++] = s2[index];
 
 	concat[len] = '\0';
