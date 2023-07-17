10e5fd6542b07ba77116c6ba6e9faee934a79b92
valebuyondo <buyondovale@gmail.com>
2023-07-17 07:36:27 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,10 @@
 #ifndef sum_h
 #define sum_h
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
 #define SUM(x, y) ((x)+(y))
+
 #endif
