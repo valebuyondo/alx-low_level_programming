b36a6a3fdbce0ca4bbeb886947ee1de83fe4f862
valebuyondo <buyondovale@gmail.com>
2023-07-17 07:15:58 -0700

    betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the file name from which its compiled
+ * Return: Always 0
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
